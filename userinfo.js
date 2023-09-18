505d8cf780b18ed27d2dc13727fc085de9998bae
shamal <shamalsundar2000@gmail.com>
2023-09-18 18:28:36 +0530

    first commit

---
+++ userinfo.js
@@ -0,0 +1,11 @@
+module.exports=[{
+    id:1,
+    username:"arshad@kallarakal",
+    name:"arshad",
+    email:"arshadkallarakal@gmail.com"
+},{
+    id:2,
+    username:"akhil@mannur",
+    name:"akhil",
+    email:"akhilmannur@gmail.com"
+}]
